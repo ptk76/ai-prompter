f046f6df21e545f981b32c773c12960d68b12b78
Przemek Kudla <pkudla@opera.com>
2025-10-11 21:01:23 +0200

    EXT-25: Show the small icon when selections has started in a text field

--- src/content_scripts/main.ts
+++ src/content_scripts/main.ts
@@ -3,6 +3,103 @@ import type { ButtonProps } from "./button";
 import createToolbar from "./toolbar";
 import getSettingButtons from "./settings";
 
+class AreteRootNode {
+  private targetHit: HTMLElement | null = null;
+  private root;
+  private onClose;
+
+  constructor(root: ShadowRoot, onClose: () => void) {
+    this.root = root;
+    this.onClose = onClose;
+    document.onmousedown = this.onMouseDown.bind(this);
+    document.onkeydown = this.onKeyDown.bind(this);
+    document.onmouseup = this.onMouseUp.bind(this);
+  }
+
+  private onKeyDown() {
+    this.closeToolbar();
+  }
+
+  private onMouseDown(ev: Event) {
+    if (this.isAiPrompter(ev.target as HTMLElement)) return;
+    this.closeToolbar();
+    this.targetHit = ev.target as HTMLElement;
+  }
+
+  private async onMouseUp(ev: MouseEvent) {
+    const target = ev.target as HTMLElement;
+    const targetHitCopy = this.targetHit != null ? this.targetHit : target;
+    this.targetHit = null;
+    if (this.isAiPrompter(target)) return;
+    const selection = window.getSelection();
+    if (!selection) return;
+
+    const selectedText = selection.toString();
+    if (!selectedText || selectedText === "") return;
+
+    if (await this.doNotShow()) return;
+
+    const settingButtons = await this.getButtons();
+    if (settingButtons.length === 0) return;
+
+    let buttons: ButtonProps[] = [];
+    // const onClose = this.onClose.bind(this);
+    for (const button of settingButtons) {
+      if (button.disabled) continue;
+      buttons.push({
+        icon: button.icon,
+        label: button.label,
+        url: button.url,
+        cb: (url: string) => {
+          this.closeToolbar();
+          Actions.callback(url, selectedText);
+        },
+      });
+    }
+    if (buttons.length === 0) return;
+    document.body.append(rootNode);
+
+    const iconOnly =
+      targetHitCopy.nodeName === "INPUT" ||
+      targetHitCopy.nodeName === "TEXTAREA";
+    const targetBox = targetHitCopy.getBoundingClientRect();
+    const x = iconOnly ? targetBox.right - 30 : ev.pageX;
+    const y = iconOnly ? window.scrollY + targetBox.top + 4 : ev.pageY;
+
+    createToolbar(this.root, x, y, buttons, iconOnly);
+  }
+
+  private isAiPrompter(element: HTMLElement | null, depth = 0): boolean {
+    if (depth === 5) return false;
+    if (!element) return false;
+    if (element.id === rootNodeId) return true;
+    return this.isAiPrompter(element.parentElement, depth + 1);
+  }
+
+  private async doNotShow() {
+    if (window.location.href.includes("docs.google.com/spreadsheets"))
+      return true;
+    return false;
+  }
+
+  private async getButtons() {
+    const settingButtons = await getSettingButtons();
+    if (settingButtons.length === 0) {
+      const responce = await chrome.runtime.sendMessage({
+        type: "fix-settings",
+      });
+      if (responce !== "settings-ready") return [];
+      return await getSettingButtons();
+    }
+    return settingButtons;
+  }
+  private closeToolbar() {
+    this.root.innerHTML = "";
+    this.targetHit = null;
+    this.onClose();
+  }
+}
+
 const rootNodeId = "arete-ai-prompter-root";
 const rootNode = document.createElement("arete-ai-prompter-root");
 rootNode.id = rootNodeId;
@@ -11,78 +108,7 @@ rootNode.style.zIndex = "1000";
 rootNode.style.top = "0px";
 rootNode.style.left = "0px";
 rootNode.style.userSelect = "none";
-const shadowRoot = rootNode.attachShadow({ mode: "open" });
 
-function isAiPrompter(element: HTMLElement | null, depth = 0) {
-  if (depth === 5) return false;
-  if (!element) return false;
-  if (element.id === rootNodeId) return true;
-  return isAiPrompter(element.parentElement, depth + 1);
-}
-
-async function doNotShow() {
-  if (window.location.href.includes("docs.google.com/spreadsheets"))
-    return true;
-  return false;
-}
-
-export function closeToolbar() {
+new AreteRootNode(rootNode.attachShadow({ mode: "open" }), () => {
   rootNode.remove();
-  shadowRoot.innerHTML = "";
-}
-
-document.onmousedown = (ev) => {
-  if (isAiPrompter(ev.target as HTMLElement)) return;
-  closeToolbar();
-};
-
-async function getButtons() {
-  const settingButtons = await getSettingButtons();
-  if (settingButtons.length === 0) {
-    const responce = await chrome.runtime.sendMessage({ type: "fix-settings" });
-    if (responce !== "settings-ready") return [];
-    return await getSettingButtons();
-  }
-  return settingButtons;
-}
-
-document.onmouseup = async (ev: MouseEvent) => {
-  const target = ev.target as HTMLElement;
-  if (isAiPrompter(target)) return;
-  const selection = window.getSelection();
-  if (!selection) return;
-
-  const selectedText = selection.toString();
-  if (!selectedText || selectedText === "") return;
-
-  if (await doNotShow()) return;
-
-  const settingButtons = await getButtons();
-  if (settingButtons.length === 0) return;
-
-  let buttons: ButtonProps[] = [];
-  for (const button of settingButtons) {
-    if (button.disabled) continue;
-    buttons.push({
-      icon: button.icon,
-      label: button.label,
-      url: button.url,
-      cb: (url: string) => {
-        closeToolbar();
-        Actions.callback(url, selectedText);
-      },
-    });
-  }
-  if (buttons.length === 0) return;
-  document.body.append(rootNode);
-  const iconOnly =
-    target.nodeName === "INPUT" || target.nodeName === "TEXTAREA";
-  const targetBox = target.getBoundingClientRect();
-  const x = iconOnly ? targetBox.right - 30 : ev.pageX;
-  const y = iconOnly ? window.scrollY + targetBox.top + 4 : ev.pageY;
-  createToolbar(shadowRoot, x, y, buttons, iconOnly);
-};
-
-document.onkeydown = () => {
-  closeToolbar();
-};
+});
